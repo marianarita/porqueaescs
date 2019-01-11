2d33ba471a6480bb31d51638666bd988bcce393a
Edite Fernandes <editeccfernandes@gmail.com>
2019-01-11 20:39:36 +0000

    Toques finais #1

--- js/main.js
+++ js/main.js
@@ -31,11 +31,16 @@ $(document).ready(function () {
         //console.log(id)
         //debugger;   
         $('[data-sectionid="' + id + '"]').addClass('visible');
-
         $('[data-section="' + id + '"]').attr('data-color');
         $('a').css({ color: $('[data-section="' + id + '"]').attr('data-color') })
         $('.sidenav-item').css({ backgroundColor: $('[data-section="' + id + '"]').attr('data-color') })
         $('[data-section="' + id + '"]').addClass('sidenav-active');
+        $('.navbar-item-active').removeClass('navbar-item-active')
+        $('.navbar').find('[href="' + '#' + id + '"]').addClass('navbar-item-active');
+        $('.current').removeClass('current');
+        $('.slidernav-active').removeClass('slidernav-active');
+        $('[data-graph="1"]').addClass('current');
+        $('[data-graph-nav="1"]').addClass('slidernav-active')
     });
 
     //Mexer o logo
@@ -60,8 +65,15 @@ $(document).ready(function () {
             $('a').css({ color: $('.current').attr('data-graphcolor') })
             $('.text-red-light').css({ color: $('.current').attr('data-graphcolor') })
             $('.slidernav-item').css({ backgroundColor: $('.current').attr('data-graphcolor') })
-        }
+            if ($(".current").next(".graph").length == 0) {
+                $('.slider-next').fadeOut(200);
+            }
+            if ($('.slider-prev').css('display') == 'none') {
+                $('.slider-prev').fadeIn(200);
+            }
+        };
     });
+    $('.slider-prev').css({ display: 'none' });
 
     $("#prev").click(function () {
         var graphid = $('.current').prev().attr('data-graph');
@@ -74,6 +86,12 @@ $(document).ready(function () {
             $('a').css({ color: $('.current').attr('data-graphcolor') })
             $('.text-red-light').css({ color: $('.current').attr('data-graphcolor') })
             $('.slidernav-item').css({ backgroundColor: $('.current').attr('data-graphcolor') })
+            if ($(".current").prev(".graph").length == 0) {
+                $('.slider-prev').fadeOut(200);
+            }
+            if ($('.slider-next').css('display') == 'none') {
+                $('.slider-next').fadeIn(200);
+            }
         }
     });
 
@@ -88,7 +106,6 @@ $(document).ready(function () {
         $('a').css({ color: $('.current').attr('data-graphcolor') })
         $('.text-red-light').css({ color: $('.current').attr('data-graphcolor') })
         $('.slidernav-item').css({ backgroundColor: $('.current').attr('data-graphcolor') })
-
     });
 
     $("#switch").change(function () {
@@ -102,7 +119,7 @@ $(document).ready(function () {
             $('.sol').stop().fadeOut(300, function () {
                 $('.lua').fadeIn()
             });
-            $('.nav-logo').css({filter: 'invert(100)'})
+            $('.nav-logo').css({ filter: 'invert(100)' })
         } else {
             $('h1, h2, h3, h4, h5, h6').css({ color: 'white' })
             $('h2').css({ color: 'white' })
@@ -113,13 +130,34 @@ $(document).ready(function () {
             $('.lua').stop().fadeOut(300, function () {
                 $('.sol').fadeIn()
             });
-            $('.nav-logo').css({filter: 'invert(0)'})
-
+            $('.nav-logo').css({ filter: 'invert(0)' })
         }
     });
 
 
-    $('#navegar').on('click', function(){
-        alert('a')
+    /* $('#navegar').on('click', function () {
+         $('.navbar').children()[1].click();
+     })*/
+
+    $('.btn').on('click', function () {
+        var sectionid = $(this).attr('href');
+        $('.navbar').find('[href="' + sectionid + '"]').click()
+
     })
+
+    $('.switch-label').click(function () {
+        if ($(this).data('count')) {
+            $(this).data('count', $(this).data('count') + 1);
+        } else {
+            $(this).data('count', 1);
+        }
+        console.log($(this).data('count'));
+        if ($(this).data('count') == 10) {
+            alert('Dum Dum DUM! Another One Bites The Dust!');
+        }
+    });
+
+
+    console.log("%c LAI'n'DO!", "font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)");
+
 });
