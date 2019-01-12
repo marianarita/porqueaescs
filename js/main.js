5e8f4c95909d6c37df00aa2e52440e4e9df07638
Edite Fernandes <editeccfernandes@gmail.com>
2019-01-12 13:03:46 +0000

    navegação com scroll e teclas

--- js/main.js
+++ js/main.js
@@ -157,7 +157,30 @@ $(document).ready(function () {
         }
     });
 
-
+    //Navegacao com teclas
+    $(document).keydown(
+        function (e) {
+            if (e.keyCode == 39 || e.keyCode == 40) {
+                //alert('direita')
+                $(".navbar-item-active").next().click();
+            }
+            if (e.keyCode == 37 || e.keyCode == 38 ) {
+               //alert('esquerda')
+                $(".navbar-item-active").prev().click();
+            }
+        }
+    );
+//scroll
+    $(window).bind('mousewheel', function (event) {
+        if (event.originalEvent.wheelDelta >= 0) {
+            //alert('Scroll cima');
+             $(".navbar-item-active").prev().click();
+        } else {
+            //alert('Scroll baixo');
+            $(".navbar-item-active").next().click();
+        }
+    });
+    
     console.log("%c L(a)INDO!", "font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)");
 
 });
